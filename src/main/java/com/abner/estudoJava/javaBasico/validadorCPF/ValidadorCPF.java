1fb77e1743618217ced9629ff0a706ec46360998
abnerAmos <abner.fsouza1@gmail.com>
2022-09-23 11:26:55 -0300

    validador de CPF Caelum Stella

---
+++ src/main/java/com/abner/estudoJava/javaBasico/validadorCPF/ValidadorCPF.java
@@ -0,0 +1,28 @@
+package com.abner.estudoJava.javaBasico.validadorCPF;
+
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+
+import java.util.List;
+
+/* TESTE: Validador de CPF Stella da Caelum */
+
+public class ValidadorCPF {
+
+    public static void main(String[] args) {
+        System.out.println(validador("12312312312"));
+    }
+
+    public static boolean validador(String cpf) {
+
+        CPFValidator cpfValidator = new CPFValidator();
+        List<ValidationMessage> erros = cpfValidator.invalidMessagesFor(cpf);
+
+        if (erros.size() > 0) {
+            System.out.println(erros);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
