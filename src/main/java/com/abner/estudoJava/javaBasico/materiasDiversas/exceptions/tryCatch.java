fde750116c66615678e382435f4c657ed6201ec1
abnerAmos <abner.fsouza1@gmail.com>
2022-12-23 09:15:19 -0300

    Exceptions

--- src/main/java/com/abner/estudoJava/javaBasico/materiasDiversas/exceptions/tryCatch.java
+++ src/main/java/com/abner/estudoJava/javaBasico/materiasDiversas/exceptions/tryCatch.java
@@ -6,12 +6,23 @@ public class tryCatch {
 
         System.out.println("Executando linha de código 1");
 
-        try {
+        try {   // Bloco que irá tentar executar
             int teste = Integer.parseInt("Hello World!");
             System.out.println("Executando linha de código 2" + teste);
 
+            /* Caso houver um erro, será tratado
+            Necessário informar o tipo que erro que pode ocorrer e que será tratado
+            Não encerra a aplicação, caso caia neste bloco.
+             */
         } catch (NumberFormatException t) {
             System.out.println("Formato Inválido!");
+
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+             /* Imprime a "Stack"(fluxo) de erro, informando todos os pontos e linhas que ocorreu o erro,
+            caso entre neste bloco
+             */
 
         } finally {
             System.out.println("Executa Independente de qualquer coisa!");
