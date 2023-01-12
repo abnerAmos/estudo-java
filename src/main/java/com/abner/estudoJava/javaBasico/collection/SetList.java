f60ac50cc724072dd941c72c1d377384d5457e98
abnerAmos <abner.fsouza1@gmail.com>
2023-01-12 15:03:27 -0300

    Att Set

--- src/main/java/com/abner/estudoJava/javaBasico/collection/SetList.java
+++ src/main/java/com/abner/estudoJava/javaBasico/collection/SetList.java
@@ -1,9 +1,6 @@
 package com.abner.estudoJava.javaBasico.collection;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetList {
 
@@ -17,8 +14,8 @@ public class SetList {
            (possui uma lógica particular na inserção de dados no meio da lista). */
 
 //        Set<String> nomes = new HashSet<>();
-//        Set<String> nomes = new TreeSet<>();
-        Set<String> nomes = new LinkedHashSet<>();
+        Set<String> nomes = new TreeSet<>();
+//        Set<String> nomes = new LinkedHashSet<>();
 
         nomes.add("Abner Amos");
         nomes.add("Abner Amos");
@@ -28,10 +25,30 @@ public class SetList {
         nomes.add("Edney Ronan");
         nomes.add("Ricardo Jonas");
         nomes.add("Ricardo Jonas");
+
+        nomes.remove("Ricardo Jonas");
+        nomes.removeIf(x -> x.charAt(0) == 'A');
 
         for (String nome : nomes) {
             System.out.println(nome);
         }
 
+        Set<Integer> numbersA = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> numbersB = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+
+        // Uniao - Mescla as duas listas em ordem, e remove os repetidos.
+        Set<Integer> numbersC = new TreeSet<>(numbersA);
+        numbersC.addAll(numbersB);
+        System.out.println(numbersC);
+
+        // Interseccao - Retorna apenas os itens repetidos entre as duas listas.
+        Set<Integer> numbersD = new TreeSet<>(numbersA);
+        numbersD.retainAll(numbersB);
+        System.out.println(numbersD);
+
+        // Diferenca - Retorna os itens da lista A que não contem na lista B.
+        Set<Integer> numbersE = new TreeSet<>(numbersA);
+        numbersE.removeAll(numbersB);
+        System.out.println(numbersE);
     }
 }
